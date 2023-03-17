054bc0612655b04ea1ccfa53382c461c395f3fc4
Conor Coker <conorcoker@gmail.com>
2023-03-17 16:06:19 +0000

    Created basic layout for homescreen which includes a bottom nav with two fragments

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.notesapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.bottomNav.setOnItemSelectedListener(this)
+        setContentView(binding.root)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem) =
+        when (item.itemId) {
+            R.id.nav_create_note -> onCreateNoteClicked()
+            R.id.nav_view_notes -> onViewNotesClicked()
+            else -> false
+        }
+
+    private fun onViewNotesClicked(): Boolean {
+        supportFragmentManager.commit { replace(R.id.fragment_container, ViewNotesFragment()) }
+        return true
+    }
+
+    private fun onCreateNoteClicked(): Boolean {
+        supportFragmentManager.commit { replace(R.id.fragment_container, CreateNoteFragment()) }
+        return true
     }
 }
+
+
+
