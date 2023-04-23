4a2f7aff0118117fa84af99d6caff68309b88773
Conor Coker <conorcoker@gmail.com>
2023-04-23 20:02:39 +0100

    Added set reminder button - this will allow user to set a reminder on their phone

--- app/src/main/java/com/example/notesapp/InDepthNoteActivity.kt
+++ app/src/main/java/com/example/notesapp/InDepthNoteActivity.kt
@@ -1,12 +1,19 @@
 package com.example.notesapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.util.Log
+import android.widget.Toast
 import com.example.notesapp.databinding.ActivityInDepthNoteBinding
 import com.example.notesapp.models.Note
 import com.example.notesapp.models.NoteJSONStore
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class InDepthNoteActivity : AppCompatActivity() {
 
@@ -19,16 +26,33 @@ class InDepthNoteActivity : AppCompatActivity() {
         binding = ActivityInDepthNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
         notes = NoteJSONStore(this)
-        note = notes.getNoteById(intent.getLongExtra("noteId",0L))!!
+        note = notes.getNoteById(intent.getLongExtra("noteId", 0L))!!
         fillNoteDetails()
-        setupDeleteButton()
+        setupButtons()
     }
 
-    private fun setupDeleteButton() {
+    @SuppressLint("SimpleDateFormat")
+    private fun setupButtons() {
 
         binding.buttonDeleteNote.setOnClickListener {
             notes.removeNoteById(note.id)
             startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        binding.buttonSetReminder.setOnClickListener {
+            val intent = Intent(Intent.ACTION_EDIT)
+            intent.type = "vnd.android.cursor.item/event"
+            val f = SimpleDateFormat("dd/MM/yyyy")
+            try {
+                val lastsUntil = f.parse(note.lastsUntil)
+                intent.putExtra("endTime", lastsUntil!!.time)
+                intent.putExtra("beginTime", Date())
+                intent.putExtra(CalendarContract.Events.TITLE, note.title)
+                startActivity(intent)
+            } catch (e: ParseException) {
+                Log.d("reminder", "caught $e when trying to set reminder")
+                Toast.makeText(this, "Something went wrong: $e", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
@@ -37,7 +61,7 @@ class InDepthNoteActivity : AppCompatActivity() {
         binding.textViewTitle.text = note.title
         binding.textViewNoteContents.text = note.noteContents
         binding.textViewDateCreatedContents.text = note.createdDate
-        if (note.isExpired()){
+        if (note.isExpired()) {
             binding.textViewLastsUntilContents.setTextColor(Color.RED)
         }
         binding.textViewLastsUntilContents.text = note.lastsUntil
